2dc690a4799ab6505be58e4ea8bb8bd7716bd944
Roberto <rdbadra@gmail.com>
2018-03-14 17:47:48 +0000

    download DeepFashion dataset

---
+++ code/managing_data/download.py
@@ -0,0 +1,10 @@
+url = "https://www.dropbox.com/sh/ryl8efwispnjw21/AACt2dLasqSDsCf-kcQwoWyfa?dl=1"
+import urllib.request
+import os
+
+def downloadDeepFashionZip():
+    currentPath = os.getcwd()
+    file_name = currentPath+"/../../DeepFashion.zip"
+    print('Downloading DeepFashion')
+    urllib.request.urlretrieve(url, file_name)  
+    print("Download Finished")
